8289ea259db37dd7d5df142a82b527be7a4907eb
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-12 22:50:21 -0300

    added "blank" info to each page

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -6,7 +6,7 @@ import './Create.css'
 export default function Create() {
   return (
     <div>
-      
+      Create
     </div>
   )
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -6,7 +6,7 @@ import './Login.css'
 export default function Login() {
   return (
     <div>
-      
+      Login
     </div>
   )
 }
