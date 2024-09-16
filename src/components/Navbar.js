3a545f031a06c95b092c5a4c7b7fd4d17b9cff27
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-15 21:58:22 -0300

    added conditional navbar links depending on user login status

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 // styles & imagems
 import './Navbar.css'
@@ -8,6 +9,7 @@ import Logo from '../assets/logo.svg'
 
 export default function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext()
 
   return (
     <div className="navbar">
@@ -19,13 +21,21 @@ export default function Navbar() {
             />
             <span>FlowNest</span>
         </li>
+      {!user && (
+        <>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Sign up</Link></li>
+        </>
+      )}
 
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Sign up</Link></li>
-        <li>
-          {!isPending && <button className='btn' onClick={logout}>Logout</button>}
-          {isPending && <button className='btn' disabled>Logging out...</button>}
-        </li>
+      {user && (
+        <>
+          <li>
+            {!isPending && <button className='btn' onClick={logout}>Logout</button>}
+            {isPending && <button className='btn' disabled>Logging out...</button>}
+          </li>
+        </>
+      )}
       </ul>
     </div>
   )
