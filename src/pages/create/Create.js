16bf17b70e1a698d64835d8f23eb1e42dacd0f0b
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-16 21:50:03 -0300

    added form errors fields

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -23,6 +23,7 @@ export default function Create() {
   const [dueDate, setDueDate] = useState('')
   const [category, setCategory] = useState('')
   const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
 
   useEffect(() => {
     if(documents) {
@@ -35,6 +36,17 @@ export default function Create() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setFormError(null)
+
+    if (!category) {
+      setFormError('Please select a project category')
+      return
+    }
+    if (assignedUsers.length < 1) {
+      setFormError('Please sign the project to at least 1 user')
+      return
+    }
+
     console.log(name, details, dueDate, category.value, assignedUsers)
   }
 
@@ -86,6 +98,8 @@ export default function Create() {
         </label>
 
         <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   )
