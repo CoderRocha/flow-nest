d803ea3932e776be44cf5c99e267282644963f49
CoderRocha <guilhermerochas0420@gmail.com>
2024-09-19 13:39:01 -0300

    fixed to not show 'Sidebar' component when user is not logged in

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
       {authIsReady && (
         <BrowserRouter>
-          <Sidebar />
+          {user ? <Sidebar element={<Dashboard />} /> : ''}
           <div className="container">
             <Navbar />
             <Routes>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,24 +15,24 @@ export default function Sidebar() {
     <div className="sidebar">
       <div className="sidebar-content">
         <div className="user">
-        {!user ? ' ' : <Avatar src={user.photoURL} />}    
-        {!user ? <p>Welcome to FlowNest!</p> : <p>Hello, {user.displayName}</p>}    
-        </div>
+          <Avatar src={user.photoURL} />
+          <p>Hello, {user.displayName}</p>  
+        </div>  
         <nav className="links">
-            <ul>
-                <li>
-                    <NavLink exact to="/">
-                        <img src={DashboardIcon} alt="Dashboard Icon" />
-                        <span>Dashboard</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/create">
-                        <img src={AddIcon} alt="New Project Icon" />
-                        <span>New Project</span>
-                    </NavLink>
-                </li>
-            </ul>
+          <ul>
+            <li>
+              <NavLink exact to="/">
+                <img src={DashboardIcon} alt="dashboard icon" />
+                <span>Dashboard</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/create">
+                <img src={AddIcon} alt="add project icon" />
+                <span>New Project</span>
+              </NavLink>
+            </li>
+          </ul>
         </nav>
       </div>
     </div>
