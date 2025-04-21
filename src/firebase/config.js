4d13c03b1adbfcd451bfe8c6ed0158092950eded
Guilherme <guilhermerochas0420@gmail.com>
2025-04-20 22:25:25 -0300

    updated firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 import 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjdVbd4B767ZCEwxBlx6voDjoHstioehU",
-    authDomain: "flow-nest.firebaseapp.com",
-    projectId: "flow-nest",
-    storageBucket: "flow-nest.appspot.com",
-    messagingSenderId: "161443949071",
-    appId: "1:161443949071:web:063ba3c8915bf54ad0917a"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
 }
 
 // initialize firebase
